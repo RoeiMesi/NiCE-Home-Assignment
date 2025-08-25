2c18b84e9ff15d26acfc5b7d6fa641d9d2fd34ae
Roei Mesilaty <RoeiM442@gmail.com>
2025-08-25 22:49:38 +0300

    Added Validator that uses FluentValidator to apply some rules on the fields in the requests, and added a controller that takes the request as defined in the model and the taskDictionary from the services that was initialized in Program.cs and returned response according to the model given

--- NiCE-Home-Assignment/Program.cs
+++ NiCE-Home-Assignment/Program.cs
@@ -1,11 +1,28 @@
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using NiCE_Home_Assignment.Models.Domain;
+using NiCE_Home_Assignment.Validation;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddFluentValidationAutoValidation();
+builder.Services.AddValidatorsFromAssemblyContaining<UserDetailsValidator>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var taskDictionary = new Dictionary<string, string>
+{
+ { "reset password", "ResetPasswordTask" },
+ { "forgot password", "ResetPasswordTask" },
+ { "check order", "CheckOrderStatusTask" },
+ { "track order", "CheckOrderStatusTask" }
+};
+
+builder.Services.AddSingleton<IDictionary<string, string>>(taskDictionary);
 
 var app = builder.Build();
 
